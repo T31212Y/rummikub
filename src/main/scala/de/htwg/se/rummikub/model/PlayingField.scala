8b18df55df33b48e9403c96d924e84d1b61b1a01
T31212Y <im.fa896@gmail.com>
2025-04-21 18:27:29 +0200

    created class Board.scala, changed method updatePlayingField and toString in PlayingField.scala and created methods lineWithPlayerName and lineWith2PlayerNames in PlayingField.scala

--- src/main/scala/de/htwg/se/rummikub/model/PlayingField.scala
+++ src/main/scala/de/htwg/se/rummikub/model/PlayingField.scala
@@ -3,8 +3,8 @@ package de.htwg.se.rummikub.model
 case class PlayingField(amountOfPlayers: Int, players: List[Player]) {
 
   val cntRows = 20
-  var cntCols = if (amountOfPlayers > 2) 140 else 80
-  val cntSpaces = 24
+  val cntTokens = 24
+  val cntEdgeSpaces = 15
  
   val player1 = players(0)
   val player2 = players(1)
@@ -12,122 +12,86 @@ case class PlayingField(amountOfPlayers: Int, players: List[Player]) {
   val player3 = if (amountOfPlayers > 2) players(2) else new Player("", List())
   val player4 = if (amountOfPlayers > 3) players(3) else new Player("", List())
 
-  val edgeUpDown = "*" * (cntCols - 1) + "\n"
+  var boardP1 = new Board(cntEdgeSpaces, cntTokens, amountOfPlayers, 1, "up")
+  var boardP2 = new Board(cntEdgeSpaces, cntTokens, amountOfPlayers, 1, "down")
 
-  var boardELRP1_1 = "|" + (" " * ((cntCols - 3) / 2 - cntSpaces)) + "| " + ("x " * (cntSpaces - 1)) + "|" + (" " * ((cntCols - 3) / 2 - cntSpaces)) + "|\n"
-  var boardELRP1_2 = "|" + (" " * ((cntCols - 3) / 2 - cntSpaces)) + "| " + ("x " * (cntSpaces - 1)) + "|" + (" " * ((cntCols - 3) / 2 - cntSpaces)) + "|\n"
-  val boardEUP1 = "|" + (" " * ((cntCols - 3) / 2 - cntSpaces)) + "+" + ("-" * (2 * cntSpaces - 1)) + "+" + (" " * ((cntCols - 3) / 2 - cntSpaces)) + "|\n"
-
-  var boardELRP13_1 = "|" + (" " * ((cntCols - 63) / 2 - cntSpaces - 1)) + "| " + ("x " * (cntSpaces - 1)) + "|" + (" " * ((cntCols - 63) / 2 - cntSpaces - 1)) + "| " + ("y " * (cntSpaces - 1)) + "|" + (" " * ((cntCols - 63) / 2 - cntSpaces - 1)) + "|\n"
-  var boardELRP13_2 = "|" + (" " * ((cntCols - 63) / 2 - cntSpaces - 1)) + "| " + ("x " * (cntSpaces - 1)) + "|" + (" " * ((cntCols - 63) / 2 - cntSpaces - 1)) + "| " + ("y " * (cntSpaces - 1)) + "|" + (" " * ((cntCols - 63) / 2 - cntSpaces - 1)) + "|\n"
-  val boardEUP13 = "|" + (" " * ((cntCols - 63) / 2 - cntSpaces - 1)) + "+" + ("-" * (2 * cntSpaces - 1)) + "+" + (" " * ((cntCols - 63) / 2 - cntSpaces - 1)) + "+" + ("-" * (2 * cntSpaces - 1)) + "+" + (" " * ((cntCols - 63) / 2 - cntSpaces - 1)) + "|\n"
-
-  val innerField = "|" + (" " * (cntCols - 3)) + "|\n"
-
-  val boardEUP2 = "|" + (" " * ((cntCols - 3) / 2 - cntSpaces)) + "+" + ("-" * (2 * cntSpaces - 1)) + "+" + (" " * ((cntCols - 3) / 2 - cntSpaces)) + "|\n"
-  var boardELRP2_1 = "|" + (" " * ((cntCols - 3) / 2 - cntSpaces)) + "| " + ("x " * (cntSpaces - 1)) + "|" + (" " * ((cntCols - 3) / 2 - cntSpaces)) + "|\n"
-  var boardELRP2_2 = "|" + (" " * ((cntCols - 3) / 2 - cntSpaces)) + "| " + ("x " * (cntSpaces - 1)) + "|" + (" " * ((cntCols - 3) / 2 - cntSpaces)) + "|\n"
-
-  val boardEUP24 = "|" + (" " * ((cntCols - 63) / 2 - cntSpaces - 1)) + "+" + ("-" * (2 * cntSpaces - 1)) + "+" + (" " * ((cntCols - 63) / 2 - cntSpaces - 1)) + "+" + ("-" * (2 * cntSpaces - 1)) + "+" + (" " * ((cntCols - 63) / 2 - cntSpaces - 1)) + "|\n"
-  var boardELRP24_1 = "|" + (" " * ((cntCols - 63) / 2 - cntSpaces - 1)) + "| " + ("x " * (cntSpaces - 1)) + "|" + (" " * ((cntCols - 63) / 2 - cntSpaces - 1)) + "| " + ("y " * (cntSpaces - 1)) + "|" + (" " * ((cntCols - 63) / 2 - cntSpaces - 1)) + "|\n"
-  var boardELRP24_2 = "|" + (" " * ((cntCols - 63) / 2 - cntSpaces - 1)) + "| " + ("x " * (cntSpaces - 1)) + "|" + (" " * ((cntCols - 63) / 2 - cntSpaces - 1)) + "| " + ("y " * (cntSpaces - 1)) + "|" + (" " * ((cntCols - 63) / 2 - cntSpaces - 1)) + "|\n"
+  var boardP13 = new Board(cntEdgeSpaces, cntTokens, amountOfPlayers, 2, "up")
+  var boardP24 = new Board(cntEdgeSpaces, cntTokens, amountOfPlayers, 2, "down")
 
   def updatePlayingField(): PlayingField = {
     var pf = new PlayingField(amountOfPlayers, players)
     if (amountOfPlayers == 2) {
-      var iP1 = 0
-      pf.player1.tokens.foreach { token => if (iP1 < 24) {
-        pf.boardELRP1_1 = pf.boardELRP1_1.replaceFirst("x", token.toString())
-      } else {
-        pf.boardELRP1_2 = pf.boardELRP1_2.replaceFirst("x", token.toString())
-      }
-        iP1 += 1        
-      }
+      pf.boardP1.boardELRP12_1 = pf.boardP1.formatBoardRow(pf.player1.tokens)
+      pf.boardP1.boardELRP12_2 = pf.boardP1.formatEmptyBoardRow(pf.boardP1.size(pf.boardP1.boardELRP12_1) - 4)
+      pf.boardP1.boardEUD = pf.boardP1.createBoardFrameSingle(pf.player1.tokens)
 
-      var iP2 = 0
-      pf.player2.tokens.foreach { token => if (iP2 < 24) {
-        pf.boardELRP2_1 = pf.boardELRP2_1.replaceFirst("x", token.toString())
-      } else {
-        pf.boardELRP2_2 = pf.boardELRP2_2.replaceFirst("x", token.toString())
-      }
-        iP2 += 1
-      }
+      pf.boardP2.boardELRP12_1 = pf.boardP2.formatBoardRow(pf.player2.tokens)
+      pf.boardP2.boardELRP12_2 = pf.boardP2.formatEmptyBoardRow(pf.boardP2.size(pf.boardP2.boardELRP12_1) - 4)
+      pf.boardP2.boardEUD = pf.boardP2.createBoardFrameSingle(pf.player2.tokens)
+
     } else if (amountOfPlayers == 3) {
-      var iP1 = 0
-      pf.player1.tokens.foreach { token => if (iP1 < 24) {
-        pf.boardELRP13_1 = pf.boardELRP13_1.replaceFirst("x", token.toString())
-      } else {
-        pf.boardELRP13_2 = pf.boardELRP13_2.replaceFirst("x", token.toString())
-      }
-        iP1 += 1
-      }
+      pf.boardP13.boardELRP12_1 = pf.boardP13.formatBoardRow(pf.player1.tokens)
+      pf.boardP13.boardELRP34_1 = pf.boardP13.formatBoardRow(pf.player3.tokens)
+      pf.boardP13.boardELRP12_2 = pf.boardP13.formatEmptyBoardRow(pf.boardP13.size(pf.boardP13.boardELRP12_1) - 4)
+      pf.boardP13.boardELRP34_2 = pf.boardP13.formatEmptyBoardRow(pf.boardP13.size(pf.boardP13.boardELRP34_1) - 4)
+      pf.boardP13.boardEUD = pf.boardP13.createBoardFrameDouble(pf.player1.tokens, pf.player3.tokens)
 
-      var iP2 = 0
-      pf.player2.tokens.foreach { token => if (iP2 < 24) {
-        pf.boardELRP2_1 = pf.boardELRP2_1.replaceFirst("x", token.toString())
-      } else {
-        pf.boardELRP2_2 = pf.boardELRP2_2.replaceFirst("x", token.toString())
-      }
-        iP2 += 1
-      }
+      pf.boardP2.maxLen = pf.boardP13.size(pf.boardP13.wrapBoardRowDouble(pf.boardP13.boardELRP12_1, pf.boardP13.boardELRP34_1)) - pf.boardP2.size(pf.boardP2.wrapBoardRowSingle(pf.boardP2.boardELRP12_1))
 
-      var iP3 = 0
-      pf.player3.tokens.foreach { token => if (iP3 < 24) {
-        pf.boardELRP13_1 = pf.boardELRP13_1.replaceFirst("y", token.toString())
-      } else {
-        pf.boardELRP13_2 = pf.boardELRP13_2.replaceFirst("y", token.toString())
-      }
-        iP3 += 1
-      }
+      pf.boardP2.boardELRP12_1 = pf.boardP2.formatBoardRow(pf.player2.tokens)
+      pf.boardP2.boardELRP12_2 = pf.boardP2.formatEmptyBoardRow(pf.boardP2.size(pf.boardP2.boardELRP12_1) - 4)
+      pf.boardP2.boardEUD = pf.boardP2.createBoardFrameSingle(pf.player2.tokens)
+
     } else if (amountOfPlayers == 4) {
-      var iP1 = 0
-      pf.player1.tokens.foreach { token => if (iP1 < 24) {
-        pf.boardELRP13_1 = pf.boardELRP13_1.replaceFirst("x", token.toString())
-      } else {
-        pf.boardELRP13_2 = pf.boardELRP13_2.replaceFirst("x", token.toString())
-      }
-        iP1 += 1
-      }
+      pf.boardP13.boardELRP12_1 = pf.boardP13.formatBoardRow(pf.player1.tokens)
+      pf.boardP13.boardELRP34_1 = pf.boardP13.formatBoardRow(pf.player3.tokens)
+      pf.boardP13.boardELRP12_2 = pf.boardP13.formatEmptyBoardRow(pf.boardP13.size(pf.boardP13.boardELRP12_1) - 4)
+      pf.boardP13.boardELRP34_2 = pf.boardP13.formatEmptyBoardRow(pf.boardP13.size(pf.boardP13.boardELRP34_1) - 4)
+      pf.boardP13.boardEUD = pf.boardP13.createBoardFrameDouble(pf.player1.tokens, pf.player3.tokens)
 
-      var iP2 = 0
-      pf.player2.tokens.foreach { token => if (iP2 < 24) {
-        pf.boardELRP24_1 = pf.boardELRP24_1.replaceFirst("x", token.toString())
-      } else {
-        pf.boardELRP24_2 = pf.boardELRP24_2.replaceFirst("x", token.toString())
-      }
-        iP2 += 1
-      }
-
-      var iP3 = 0
-      pf.player3.tokens.foreach { token => if (iP3 < 24) {
-        pf.boardELRP13_1 = pf.boardELRP13_1.replaceFirst("y", token.toString())
-      } else {
-        pf.boardELRP13_2 = pf.boardELRP13_2.replaceFirst("y", token.toString())
-      }
-        iP3 += 1
-      }
-
-      var iP4 = 0
-      pf.player4.tokens.foreach { token => if (iP4 < 24) {
-        pf.boardELRP24_1 = pf.boardELRP24_1.replaceFirst("y", token.toString())
-      } else {
-        pf.boardELRP24_2 = pf.boardELRP24_2.replaceFirst("y", token.toString())
-      }
-        iP4 += 1
-      }
+      pf.boardP24.boardELRP12_1 = pf.boardP24.formatBoardRow(pf.player2.tokens)
+      pf.boardP24.boardELRP34_1 = pf.boardP24.formatBoardRow(pf.player4.tokens)
+      pf.boardP24.boardELRP12_2 = pf.boardP24.formatEmptyBoardRow(pf.boardP24.size(pf.boardP24.boardELRP12_1) - 4)
+      pf.boardP24.boardELRP34_2 = pf.boardP24.formatEmptyBoardRow(pf.boardP24.size(pf.boardP24.boardELRP34_1) - 4)
+      pf.boardP24.boardEUD = pf.boardP24.createBoardFrameDouble(pf.player2.tokens, pf.player4.tokens)
     }
     pf
   }
 
+  def lineWithPlayerName(char: String, length: Int, p: String): String = {
+    val len = length - p.length - 5
+    char.repeat(5) + p + char * len
+  }
+
+  def lineWith2PlayerNames(char: String, length: Int, p1: String, p2: String): String = {
+    val len = length - p1.length - p2.length - 10
+    char.repeat(5) + p1 + char * len + p2 + char.repeat(5)
+  }
+
+  def simpleLine(char: String, length: Int): String = {
+    char * length
+  }
+
   override def toString(): String = {
     var playingField = ""
+    if (amountOfPlayers == 2) { 
+      val edgeUp = lineWithPlayerName("*", boardP1.size(boardP1.wrapBoardRowSingle(boardP1.boardELRP12_1)), player1.name) + "\n"
+      val innerField = "|" + simpleLine(" ", boardP1.size(boardP1.wrapBoardRowSingle(boardP1.boardELRP12_1)) - 2) + "|\n"
+      val edgeDown = lineWithPlayerName("*", boardP1.size(boardP1.wrapBoardRowSingle(boardP1.boardELRP12_1)), player2.name) + "\n"
 
-    if (amountOfPlayers == 2) {
-      playingField = " ".repeat(15) + "***" + player1.name + "\n" + edgeUpDown + boardELRP1_1 + boardEUP1 + boardELRP1_2 + boardEUP1 + innerField.repeat(cntRows - 4) + boardEUP2 + boardELRP2_1 + boardEUP2 + boardELRP2_2 + edgeUpDown + " ".repeat(15) + "***" + player2.name + "\n"
+      playingField = edgeUp + boardP1.toString() + innerField.repeat(cntRows - 4) + boardP2.toString() + edgeDown + "\n"
     } else if (amountOfPlayers == 3) {
-      playingField = " ".repeat(14) + "***" + player1.name + " ".repeat(55) + "***" + player2.name + "\n" + edgeUpDown + boardELRP13_1 + boardEUP13 + boardELRP13_2 + boardEUP13 + innerField.repeat(cntRows - 4) + boardEUP2 + boardELRP2_1 + boardEUP2 + boardELRP2_2 + edgeUpDown + " ".repeat(45) + "***" + player3.name + "\n"
+      val edgeUp = lineWith2PlayerNames("*", boardP13.size(boardP13.wrapBoardRowDouble(boardP13.boardELRP12_1, boardP13.boardELRP34_1)), player1.name, player3.name) + "\n"
+      val innerField = "|" + simpleLine(" ", boardP13.size(boardP13.wrapBoardRowDouble(boardP13.boardELRP12_1, boardP13.boardELRP34_1)) - 2) + "|\n"
+      val edgeDown = lineWithPlayerName("*", boardP13.size(boardP13.wrapBoardRowDouble(boardP13.boardELRP12_1, boardP13.boardELRP34_1)), player2.name) + "\n"
+
+      playingField = edgeUp + boardP13.toString() + innerField.repeat(cntRows - 4) + boardP2.toString() + edgeDown + "\n"
     } else if (amountOfPlayers == 4) {
-      playingField = " ".repeat(14) + "***" + player1.name + " ".repeat(55) + "***" + player2.name + "\n" + edgeUpDown + boardELRP13_1 + boardEUP13 + boardELRP13_2 + boardEUP13 + innerField.repeat(cntRows - 4) + boardEUP24 + boardELRP24_1 + boardEUP24 + boardELRP24_2 + edgeUpDown + " ".repeat(14) + "***" + player3.name + " ".repeat(55) + "***" + player4.name + "\n"
+      val edgeUp = lineWith2PlayerNames("*", boardP13.size(boardP13.wrapBoardRowDouble(boardP13.boardELRP12_1, boardP13.boardELRP34_1)), player1.name, player3.name) + "\n"
+      val innerField = "|" + simpleLine(" ", boardP13.size(boardP13.wrapBoardRowDouble(boardP13.boardELRP12_1, boardP13.boardELRP34_1)) - 2) + "|\n"
+      val edgeDown = lineWith2PlayerNames("*", boardP24.size(boardP24.wrapBoardRowDouble(boardP24.boardELRP12_1, boardP24.boardELRP34_1)), player2.name, player4.name) + "\n"
+
+      playingField = edgeUp + boardP13.toString() + innerField.repeat(cntRows - 4) + boardP24.toString() + edgeDown + "\n"
     }
 
     playingField
