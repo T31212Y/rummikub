f0e72548d7a5f043b79ca749636efd6a36630f76
T31212Y <im.fa896@gmail.com>
2025-04-29 20:11:39 +0200

    finished implementing controller-methods in Controller.scala

--- src/main/scala/de/htwg/se/rummikub/controller/Controller.scala
+++ src/main/scala/de/htwg/se/rummikub/controller/Controller.scala
@@ -4,12 +4,27 @@ import de.htwg.se.rummikub.model.{Player, PlayingField, TokenStack, Row, Group, 
 import de.htwg.se.rummikub.util.Observable
 
 class Controller(var playingField: PlayingField) extends Observable {
+
     def createPlayingField(amountOfPlayers: Int, players: List[Player]): Unit = {
         playingField = PlayingField(amountOfPlayers, players)
         notifyObservers
     }
 
-    def getPlayingField: PlayingField = playingField
+    def createTokenStack(): TokenStack = {
+        TokenStack()
+    }
+
+    def createPlayer(name: String): Player = {
+        Player(name)
+    }
+
+    def createRow(r: List[String]): Row = {
+        Row(r)
+    }
+
+    def createGroup(g: List[String]): Group = {
+        Group(g)
+    }
 
     def setPlayingField(playingField: PlayingField): Unit = {
         this.playingField = playingField
@@ -17,15 +32,15 @@ class Controller(var playingField: PlayingField) extends Observable {
     }
 
     def playingfieldToString: String = {
-        playingField.toString()
+        playingField.toString().replace("x", " ").replace("y", " ")
     }
 
-    def addTokenToPlayer(player: Player, token: Token | Joker): Unit = {
+    def addTokenToPlayer(player: Player, stack: TokenStack): Unit = {
+        val tokenToAdd = stack.drawToken()
         playingField = playingField.copy(players = playingField.players.map {
-            case p if p.name == player.name => p.copy(tokens = p.tokens :+ token)
+            case p if p.name == player.name => p.copy(tokens = p.tokens :+ tokenToAdd)
             case p => p
         })
-        notifyObservers
     }
 
     def removeTokenFromPlayer(player: Player, token: Token | Joker): Unit = {
@@ -33,13 +48,66 @@ class Controller(var playingField: PlayingField) extends Observable {
             case p if p.name == player.name => p.copy(tokens = p.tokens.filterNot(_.equals(token)))
             case p => p
         })
-        notifyObservers
-    }
-    
-    def addPlayer(name: String): Unit = {
-        val player = Player(name)
-        playingField = playingField.copy(players = playingField.players :+ player)
-        notifyObservers
     }
 
+    def addMultipleTokensToPlayer(player: Player, stack: TokenStack, amt: Int): Unit = {
+        val tokensToAdd = stack.drawMultipleTokens(amt)
+        playingField = playingField.copy(players = playingField.players.map {
+            case p if p.name == player.name => p.copy(tokens = p.tokens ++ tokensToAdd)
+            case p => p
+        })
+    }
+    
+    def passTurn(currentPlayer: Player): Player = {
+        val nextPlayer = setNextPlayer(currentPlayer).copy(commandHistory = List(""))
+        playingField = playingField.copy(players = playingField.players.map {
+            case p if p.name == nextPlayer.name => nextPlayer
+            case p => p
+        })
+        println(currentPlayer.name + " passed the turn to " + nextPlayer.name)
+        nextPlayer
+    }
+
+    def setNextPlayer(p: Player): Player = {
+        val current = playingField.players.indexWhere(_.name == p.name)
+        if (current == playingField.players.size - 1) playingField.players.head else playingField.players(current + 1)
+    }
+
+    def winGame(): Boolean = {
+        playingField.players.exists(player => player.tokens.isEmpty) match {
+            case true => {
+                println("Player " + playingField.players.find(_.tokens.isEmpty).get.name + " wins the game!")
+                true
+            }
+            case false => false
+        }
+    }
+
+    def addRowToTable(row: Row): List [Token | Joker] = {
+        val updatedPlayingField = {
+            if (playingField.amountOfPlayers == 2) {
+                playingField.copy(innerField2Players = playingField.innerField2Players.add(row.rowTokens))
+            } else if (playingField.amountOfPlayers > 2) {
+                playingField.copy(innerField34Players = playingField.innerField34Players.add(row.rowTokens))
+            } else {
+                playingField
+            }
+        }
+        playingField = updatedPlayingField
+        row.rowTokens
+    }
+
+    def addGroupToTable(group: Group): List[Token | Joker] = {
+        val updatedPlayingField = {
+            if (playingField.amountOfPlayers == 2) {
+                playingField.copy(innerField2Players = playingField.innerField2Players.add(group.groupTokens))
+            } else if (playingField.amountOfPlayers > 2) {
+                playingField.copy(innerField34Players = playingField.innerField34Players.add(group.groupTokens))
+            } else {
+                playingField
+            }
+        }
+        playingField = updatedPlayingField
+        group.groupTokens
+    }
 }
