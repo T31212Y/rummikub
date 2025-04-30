b8a1901216eda9b96c09fe52757e13b51b36df29
anki1103 <akvollmi@gmx.de>
2025-04-30 12:25:03 +0200

    created ControllerSpec

---
+++ src/test/scala/de/htwg/se/rummikub/controller/ControllerSpec.scala
@@ -0,0 +1,21 @@
+package de.htwg.se.rummikub.controller
+
+    /*"detect win if player has no tokens" in {
+      val players = List(Player("Anki", Nil), Player("Imma", List(Token(2, Color.RED))))
+      captureOutput {
+        tui.winGame(players) shouldBe true
+      } should include ("wins the game")
+    }
+
+    "not detect win when no player won yet" in {
+      val players = List(
+        Player("Anki", List(Token(1, Color.RED))),
+        Player("Imma", List(Token(2, Color.BLUE)))
+      )
+      tui.winGame(players) shouldBe false
+    }
+
+        "pass turn to next player" in {
+      val nextPlayer = tui.passTurn(playingField, player1)
+      nextPlayer.name shouldEqual "Imma"
+    }*/
