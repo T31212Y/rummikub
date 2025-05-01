51498ff3d76a19555afab2c137c1584f18418cc8
anki1103 <akvollmi@gmx.de>
2025-05-01 15:45:09 +0200

    created RummikubSpec and added tests

---
+++ src/test/scala/de/htwg/se/rummikub/RummikubSpec.scala
@@ -0,0 +1,53 @@
+package de.htwg.se.rummikub
+
+import de.htwg.se.rummikub.model.{Player, PlayingField}
+import de.htwg.se.rummikub.controller.Controller
+import de.htwg.se.rummikub.aview.Tui
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, PrintStream}
+
+class RummikubSpec extends AnyWordSpec with Matchers {
+  "Rummikub" should {
+    "have a main method that runs without exceptions" in {
+      noException should be thrownBy {
+        val input = new ByteArrayInputStream("quit\n".getBytes)
+        Console.withIn(input) {
+          Rummikub.main(Array())
+        }
+      }
+    }
+
+    "initialize the game correctly with default players" in {
+      val controller = Rummikub.controller
+      controller.playingField.players.map(_.name) should contain allOf ("Emilia", "Noah")
+      controller.playingField.amountOfPlayers should be(2)
+    }
+
+    "process commands through the Tui" in {
+      val input = new ByteArrayInputStream("help\nquit\n".getBytes)
+      val output = new ByteArrayOutputStream()
+      Console.withIn(input) {
+        Console.withOut(new PrintStream(output)) {
+          Rummikub.main(Array())
+        }
+      }
+      output.toString should include("Welcome to")
+      output.toString should include("Please enter a command:")
+      output.toString should include("help")
+    }
+
+    "handle invalid input gracefully" in {
+      val input = new ByteArrayInputStream("invalid\nquit\n".getBytes)
+      val output = new ByteArrayOutputStream()
+      Console.withIn(input) {
+        Console.withOut(new PrintStream(output)) {
+          Rummikub.main(Array())
+        }
+      }
+      output.toString should include("Please enter a command:")
+    }
+  }
+}
