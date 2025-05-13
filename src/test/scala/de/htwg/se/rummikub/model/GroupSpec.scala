ee32bd436147a826405c295b831d9f33e95ab1e2
anki1103 <akvollmi@gmx.de>
2025-05-13 16:43:50 +0200

    fixed GroupSpec

--- src/test/scala/de/htwg/se/rummikub/model/GroupSpec.scala
+++ src/test/scala/de/htwg/se/rummikub/model/GroupSpec.scala
@@ -10,13 +10,13 @@ class GroupSpec extends AnyWordSpec {
             val token2 = "1:blue"
             val token3 = "1:green"
             val group = Group(List(token1, token2, token3))
-            group.groupTokens should be (List(Token(1, Color.RED), Token(1, Color.BLUE), Token(1, Color.GREEN)))
+            group.groupTokens should be (List(TokenFactory.createToken("NumToken", 1, Color.RED), TokenFactory.createToken("NumToken", 1, Color.BLUE), TokenFactory.createToken("NumToken", 1, Color.GREEN)))
         }
 
         "add a token" in {
             val token = "1:red"
             val group = Group(List(token))
-            val tokenToAdd = Token(1, Color.RED)
+            val tokenToAdd = TokenFactory.createToken("NumToken", 1, Color.RED)
             group.addToken(tokenToAdd)
             group.getTokens should contain (tokenToAdd)
         }
@@ -25,7 +25,7 @@ class GroupSpec extends AnyWordSpec {
             val token1 = "11:red"
             val token2 = "11:blue"
             val token3 = "11:black"
-            val tokenToRemove = Token(11, Color.RED)
+            val tokenToRemove = TokenFactory.createToken("NumToken", 11, Color.RED)
             val group = Group(List(token1, token2, token3))
             group.removeToken(tokenToRemove)
             group.getTokens should not contain (tokenToRemove)
@@ -35,7 +35,7 @@ class GroupSpec extends AnyWordSpec {
             val token1 = "13:green"
             val token2 = "13:red"
             val token3 = "13:blue"
-            val tokenToGet = Token(13, Color.GREEN)
+            val tokenToGet = TokenFactory.createToken("NumToken", 13, Color.GREEN)
             val group = Group(List(token1, token2, token3))
             group.getTokens should contain (tokenToGet)
         }
