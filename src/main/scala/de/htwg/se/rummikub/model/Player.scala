4db3079e8349bbd59bb3df2208889b3874eca802
T31212Y <im.fa896@gmail.com>
2025-05-15 21:20:21 +0200

    added method addToFirstMoveTokens and extended construktor with firstMoveTokens, changed method validateFirstMove

--- src/main/scala/de/htwg/se/rummikub/model/Player.scala
+++ src/main/scala/de/htwg/se/rummikub/model/Player.scala
@@ -1,26 +1,26 @@
 package de.htwg.se.rummikub.model
 
-case class Player(name: String, tokens: List[Token] = List(), commandHistory: List[String] = List()) {
+case class Player(name: String, tokens: List[Token] = List(), commandHistory: List[String] = List(), firstMoveTokens: List[Token] = List()) {
+
   override def toString: String = {
     s"Player: $name"
   }
 
   def validateFirstMove(): Boolean = {
-    val totalPoints = commandHistory.collect {
-      case command if command.startsWith("row:") || command.startsWith("group:") =>
-        val tokens = command.split(":")(1).split(",").map(_.trim)
-        tokens.collect {
-          case tokenString if tokenString.contains(":") =>
-            val parts = tokenString.split(":")
-            if (parts(0).forall(_.isDigit)) parts(0).toInt else 0
-        }.sum
+    val totalPoints = firstMoveTokens.collect {
+      case NumToken(n, _) => n
+      case _: Joker       => 0
     }.sum
 
     if (totalPoints < 30) {
-      println(s"$name's first move must have a total of at least 30 points.")
+      println(s"$name's first move must have a total of at least 30 points. You only have $totalPoints.")
       false
     } else {
       true
     }
+  }
+
+  def addToFirstMoveTokens(newTokens: List[Token]): Player = {
+    this.copy(firstMoveTokens = this.firstMoveTokens ++ newTokens)
   }
 }
