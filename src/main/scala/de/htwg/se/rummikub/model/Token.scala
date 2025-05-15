6d26c65a3cce56db8b5734fc869288457bbff8c4
anki1103 <akvollmi@gmx.de>
2025-05-15 11:34:47 +0200

    splitted Token.scala in NumToken and Joker

--- src/main/scala/de/htwg/se/rummikub/model/Token.scala
+++ src/main/scala/de/htwg/se/rummikub/model/Token.scala
@@ -4,24 +4,3 @@ trait Token {
   override def toString: String
 }
 
-case class NumToken(number: Int, color: Color) extends Token {
-  override def toString: String = f"${color.toString}$number%2d${color.reset}"
-
-  override def equals(obj: Any): Boolean = obj match {
-    case that: NumToken => this.number == that.number && this.color == that.color
-    case _ => false
-  }
-
-  override def hashCode(): Int = (number, color).##
-}
-
-case class Joker(color: Color) extends Token {
-  override def toString: String = s" ${color.toString}J${color.reset}"
-
-  override def equals(obj: Any): Boolean = obj match {
-    case that: Joker => this.color == that.color
-    case _ => false
-  }
-
-  override def hashCode(): Int = color.##
-}
