0291e2d61dda9a119114e6fa0967935e04926f5f
anki1103 <akvollmi@gmx.de>
2025-05-21 15:38:20 +0200

    fixed main

--- src/main/scala/de/htwg/se/rummikub/model/Main.scala
+++ src/main/scala/de/htwg/se/rummikub/model/Main.scala
@@ -1,8 +1,11 @@
 package de.htwg.se.rummikub.model
 
 @main def run(): Unit = 
-  val gameMode = GameModeFactory.createGameMode(2, List("Azra", "Moritz"))
-  val players = gameMode.createPlayers()
-  val playingField = gameMode.createPlayingField(players)
-  
-  println(gameMode.renderPlayingField(playingField))
+  val gameModeTry = GameModeFactory.createGameMode(2, List("Azra", "Moritz"))
+  gameModeTry match
+    case scala.util.Success(gameMode) =>
+      val players = gameMode.createPlayers()
+      val playingField = gameMode.createPlayingField(players)
+      println(gameMode.renderPlayingField(playingField))
+    case scala.util.Failure(exception) =>
+      println(s"Failed to create game mode: ${exception.getMessage}")
