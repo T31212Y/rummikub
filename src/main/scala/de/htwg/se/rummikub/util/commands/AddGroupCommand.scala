4b7cbc996edb82f6b90e0cc8f3b23b327c06e27e
T31212Y <im.fa896@gmail.com>
2025-05-21 19:25:33 +0200

    added undo redo mechanism for groups

---
+++ src/main/scala/de/htwg/se/rummikub/util/commands/AddGroupCommand.scala
@@ -0,0 +1,31 @@
+package de.htwg.se.rummikub.util.commands
+
+import de.htwg.se.rummikub.model._
+import de.htwg.se.rummikub.controller.Controller
+import de.htwg.se.rummikub.util.Command
+import de.htwg.se.rummikub.state.GameState
+
+class AddGroupCommand(controller: Controller, group: Group, player: Player, stack: TokenStack) extends Command {
+
+  private val oldState: Option[GameState] = Some(controller.getState)
+  var removedTokens: List[Token] = List()
+
+  override def doStep(): Unit = {
+    val (tokensRemoved, updatedPlayer) = controller.addGroupToTable(group, player)
+    tokensRemoved.foreach(t => controller.removeTokenFromPlayer(updatedPlayer, t))
+    removedTokens = tokensRemoved
+  }
+
+  override def undoStep(): Unit = {
+    oldState match {
+      case Some(state) =>
+        controller.setStateInternal(state)
+      case None =>
+        println("No State available.")
+    }
+  }
+
+  override def redoStep(): Unit = {
+    doStep()
+  }
+}
