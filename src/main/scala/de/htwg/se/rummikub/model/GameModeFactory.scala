2cd7ed64612a0f1797e3e347949c68a678c00e89
anki1103 <akvollmi@gmx.de>
2025-05-21 11:33:58 +0200

    changed to try and option instead of throw

--- src/main/scala/de/htwg/se/rummikub/model/GameModeFactory.scala
+++ src/main/scala/de/htwg/se/rummikub/model/GameModeFactory.scala
@@ -1,9 +1,12 @@
 package de.htwg.se.rummikub.model
 
+import scala.util.{Try, Success, Failure}
+
 object GameModeFactory {
-  def createGameMode(amtPlayers: Int, playerNames: List[String]): GameModeTemplate = amtPlayers match {
-    case 2 => TwoPlayerMode(playerNames)
-    case 3 => ThreePlayerMode(playerNames)
-    case 4 => FourPlayerMode(playerNames)
+  def createGameMode(amtPlayers: Int, playerNames: List[String]): Try[GameModeTemplate] = amtPlayers match {
+    case 2 => Success(TwoPlayerMode(playerNames))
+    case 3 => Success(ThreePlayerMode(playerNames))
+    case 4 => Success(FourPlayerMode(playerNames))
+    case _ => Failure(new IllegalArgumentException("Invalid number of players"))
   }
 }
