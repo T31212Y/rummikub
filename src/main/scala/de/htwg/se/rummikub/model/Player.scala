421b366bcbe6ef583eb4a0dfa8f2ab23c8353d81
T31212Y <im.fa896@gmail.com>
2025-05-24 21:28:28 +0200

    changed method validateFirstMove to be able to check points of rows and groups with jokers as well, added method clusterTokens to simplify method validateFirstMove

--- src/main/scala/de/htwg/se/rummikub/model/Player.scala
+++ src/main/scala/de/htwg/se/rummikub/model/Player.scala
@@ -7,17 +7,19 @@ case class Player(name: String, tokens: List[Token] = List(), commandHistory: Li
   }
 
   def validateFirstMove(): Boolean = {
-    val totalPoints = firstMoveTokens.collect {
-      case NumToken(n, _) => n
-      case _: Joker       => 0
-    }.sum
+    val structures = clusterTokens(firstMoveTokens)
+
+    if (structures.isEmpty) {
+      println(s"$name's first move is invalid: cannot form valid groups or rows")
+      return false
+    }
+
+    val totalPoints = structures.map(_.points).sum
 
     if (totalPoints < 30) {
       println(s"$name's first move must have a total of at least 30 points. You only have $totalPoints.")
       false
-    } else {
-      true
-    }
+    } else true
   }
 
   def addToFirstMoveTokens(newTokens: List[Token]): Player = {
@@ -29,4 +31,35 @@ case class Player(name: String, tokens: List[Token] = List(), commandHistory: Li
     firstMoveTokens = this.firstMoveTokens.map(identity),
     commandHistory = this.commandHistory.map(identity)
   )
+
+  def clusterTokens(tokens: List[Token]): List[TokenStructure] = {
+    
+    def backtrack(remaining: List[Token], acc: List[TokenStructure]): Option[List[TokenStructure]] = {
+      if (remaining.isEmpty) {
+        Some(acc)
+      } else {
+        val minSize = 3
+        val maxSize = Math.min(remaining.size, 13)
+
+        val possibleSets = for {
+          size <- minSize to maxSize
+          subset <- remaining.combinations(size)
+        } yield {
+          val ts = subset.toList
+          val group = Group(ts)
+          val row = Row(ts)
+          List(group, row).filter(_.isValid).map(validSet => (validSet, ts))
+        }
+
+        val flatPossibleSets = possibleSets.flatten.iterator
+
+        flatPossibleSets.flatMap { case (set, ts) =>
+          val newRemaining = remaining.diff(ts)
+          backtrack(newRemaining, acc :+ set)
+        }.toSeq.headOption
+      }
+    }
+
+    backtrack(tokens, List()).getOrElse(List())
+  }
 }
