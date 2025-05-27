eb20cce7971ee1bc8a4cec0ae216532da1b27259
T31212Y <im.fa896@gmail.com>
2025-05-27 18:09:39 +0200

    changed TokenStack from java style to functional scala style

--- src/main/scala/de/htwg/se/rummikub/model/TokenStack.scala
+++ src/main/scala/de/htwg/se/rummikub/model/TokenStack.scala
@@ -2,43 +2,44 @@ package de.htwg.se.rummikub.model
 
 import scala.util.Random
 
-case class TokenStack() {
-  private val tokenFactory: TokenFactory = new StandardTokenFactory
-  val allTokens = tokenFactory.generateAllTokens()
+case class TokenStack(tokens: List[Token]) {
 
-  var tokens: List[Token] = Random.shuffle(allTokens)
-
-  def drawToken(): Token = {
+  def drawToken(): (Token, TokenStack) = {
     val token = tokens.head
-    tokens = tokens.tail
-    token
+    val updatedStack = TokenStack(tokens.tail)
+    (token, updatedStack)
   }
 
-  def drawMultipleTokens(n: Int): List[Token] = {
-    (1 to n).map(_ => drawToken()).toList
+  def drawMultipleTokens(n: Int): (List[Token], TokenStack) = {
+    val drawn = tokens.take(n)
+    val updatedStack = TokenStack(tokens.drop(n))
+    (drawn, updatedStack)
   }
 
-  def removeToken(token: Token): List[Token] = {
-    tokens = tokens.filterNot(_.equals(token))
-    tokens
+  def removeToken(token: Token): TokenStack =
+    TokenStack(tokens.filterNot(_ == token))
+
+  def removeMultipleTokens(n: Int): (List[Token], TokenStack) = {
+    val removed = tokens.take(n)
+    val updatedStack = TokenStack(tokens.drop(n))
+    (removed, updatedStack)
   }
 
-  def removeMultipleTokens(n: Int): List[Token] = {
-    val tokensToRemove = tokens.take(n)
-    tokens = tokens.drop(n)
-    tokensToRemove
-  }
-
-  def removeAllTokens(): List[Token] = {
-    tokens = List()
-    tokens
-  }
+  def removeAllTokens(): TokenStack =
+    TokenStack(List())
 
   def isEmpty: Boolean = tokens.isEmpty
 
   def size: Int = tokens.size
 
-  override def toString(): String = {
+  override def toString: String =
     tokens.map(_.toString).mkString(", ")
+}
+
+object TokenStack {
+  def apply(): TokenStack = {
+    val tokenFactory = new StandardTokenFactory
+    val allTokens = Random.shuffle(tokenFactory.generateAllTokens())
+    new TokenStack(allTokens)
   }
 }
