9f8c0195ff0d6c2ef391133060aee98d7a92ab34
anki1103 <akvollmi@gmx.de>
2025-05-28 11:19:16 +0200

    fixed errors in RowSpec

--- src/test/scala/de/htwg/se/rummikub/model/RowSpec.scala
+++ src/test/scala/de/htwg/se/rummikub/model/RowSpec.scala
@@ -6,58 +6,65 @@ import org.scalatest.matchers.should.Matchers._
 class RowSpec extends AnyWordSpec {
   "Row" should {
     "be created with a list of tokens" in {
-        val token1 = "7:blue"
-        val token2 = "8:blue"
-        val token3 = "9:blue"
-        val row = Row(List(token1, token2, token3))
         val tokenFactory = new StandardTokenFactory
-        row.getTokens should be (List(tokenFactory.createNumToken(7, Color.BLUE), tokenFactory.createNumToken(8, Color.BLUE), tokenFactory.createNumToken(9, Color.BLUE)))
+        val token1 = tokenFactory.createNumToken(7, Color.BLUE)
+        val token2 = tokenFactory.createNumToken(8, Color.BLUE)
+        val token3 = tokenFactory.createNumToken(9, Color.BLUE)
+        val row = Row(List(token1, token2, token3))
+        row.getTokens should be (List(token1, token2, token3))
     }
 
     "add a token" in {
-      val token = "1:red"
-      val row = Row(List(token))
       val tokenFactory = new StandardTokenFactory
+      val token = tokenFactory.createNumToken(1, Color.RED)
+      val row = Row(List(token))
       val tokenToAdd = tokenFactory.createNumToken(1, Color.RED)
       row.addToken(tokenToAdd)
       row.getTokens should contain (tokenToAdd)
     }
 
     "remove a token" in {
-      val token1 = "11:blue"
-      val token2 = "12:blue"
-      val token3 = "13:blue"
-      val row = Row(List(token1, token2, token3))
       val tokenFactory = new StandardTokenFactory
+      val token1 = tokenFactory.createNumToken(11, Color.BLUE)
+      val token2 = tokenFactory.createNumToken(12, Color.BLUE)
+      val token3 = tokenFactory.createNumToken(13, Color.BLUE)
+      val row = Row(List(token1, token2, token3))
       val tokenToRemove = tokenFactory.createNumToken(13, Color.BLUE)
       row.removeToken(tokenToRemove)
       row.getTokens should not contain (tokenToRemove)
     }
 
     "get tokens" in {
-      val token1 = "13:green"
-      val token2 = "1:green"
-      val token3 = "2:green"
-      val row = Row(List(token1, token2, token3))
       val tokenFactory = new StandardTokenFactory
+      val token1 = tokenFactory.createNumToken(13, Color.GREEN)
+      val token2 = tokenFactory.createNumToken(1, Color.GREEN)
+      val token3 = tokenFactory.createNumToken(2, Color.GREEN)
+      val row = Row(List(token1, token2, token3))
       val tokenToGet = tokenFactory.createNumToken(13, Color.GREEN)
       row.getTokens should contain (tokenToGet)
     }
 
     "have a correct string representation" in {
-      val token1 = "5:red"
-      val token2 = "6:red"
-      val token3 = "J:red"
-      val token4 = "J:black"
+      val tokenFactory = new StandardTokenFactory
+      val token1 = tokenFactory.createNumToken(5, Color.RED)
+      val token2 = tokenFactory.createNumToken(6, Color.RED)
+      val token3 = tokenFactory.createJoker(Color.RED)
+      val token4 = tokenFactory.createJoker(Color.BLACK)
       val row = Row(List(token1, token2, token3, token4))
       row.toString should be (row.getTokens.map(_.toString).mkString(" "))
     }
 
     "change string list to token list" in {
       val tokenStrings = List("1:red", "2:blue", "3:green", "4:black")
-      val row = Row(tokenStrings)
       val tokenFactory = new StandardTokenFactory
-      row.changeStringListToTokenList(tokenStrings) should be (List(tokenFactory.createNumToken(1, Color.RED), tokenFactory.createNumToken(2, Color.BLUE), tokenFactory.createNumToken(3, Color.GREEN), tokenFactory.createNumToken(4, Color.BLACK)))
+      val tokens = tokenStrings.map {
+        case s"$num:red"   => tokenFactory.createNumToken(num.toInt, Color.RED)
+        case s"$num:blue"  => tokenFactory.createNumToken(num.toInt, Color.BLUE)
+        case s"$num:green" => tokenFactory.createNumToken(num.toInt, Color.GREEN)
+        case s"$num:black" => tokenFactory.createNumToken(num.toInt, Color.BLACK)
+      }
+      val row = Row(tokens)
+      Row.changeStringListToTokenList(tokenStrings, tokenFactory) should be (List(tokenFactory.createNumToken(1, Color.RED), tokenFactory.createNumToken(2, Color.BLUE), tokenFactory.createNumToken(3, Color.GREEN), tokenFactory.createNumToken(4, Color.BLACK)))
     }
   }
 }
