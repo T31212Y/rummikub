fb755eaf56e298a9306a139ae941e7a4a9f48995
anki1103 <akvollmi@gmx.de>
2025-05-28 11:40:41 +0200

    fixed errors in TwoPlayerModeSpec

--- src/test/scala/de/htwg/se/rummikub/model/TwoPlayerModeSpec.scala
+++ src/test/scala/de/htwg/se/rummikub/model/TwoPlayerModeSpec.scala
@@ -83,18 +83,18 @@ class TwoPlayerModeSpec extends AnyWordSpec {
     }
 
     "A player's board should split tokens correctly when more than cntTokens are present" in {
-      val stack = new TokenStack()
-      val manyTokens = stack.drawMultipleTokens(30)
-      val player = Player("Azra", manyTokens)
+      val stack = new TokenStack(List.empty[Token])
+      val manyTokensTuple = stack.drawMultipleTokens(30)
+      val player = Player("Azra", manyTokensTuple._1)
       val board = new Board(24, 14, 2, 2, "default", 10)
 
       val updatedOpt = mode.updateBoardSinglePlayer(player, board)
       updatedOpt.isDefined shouldBe true
 
       val updatedBoard = updatedOpt.get
-      updatedBoard.boardELRP12_1 shouldBe board.formatBoardRow(manyTokens.take(mode.cntTokens))
-      updatedBoard.boardELRP12_2 shouldBe board.formatBoardRow(manyTokens.drop(mode.cntTokens))
-      updatedBoard.boardEUD shouldBe board.createBoardFrameSingle(manyTokens.take(mode.cntTokens))
+      updatedBoard.boardELRP12_1 shouldEqual board.formatBoardRow(manyTokensTuple._1.take(mode.cntTokens))
+      updatedBoard.boardELRP12_2 shouldEqual board.formatBoardRow(manyTokensTuple._1.drop(mode.cntTokens))
+      updatedBoard.boardEUD shouldEqual board.createBoardFrameSingle(manyTokensTuple._1.take(mode.cntTokens))
     }
   }
 }
