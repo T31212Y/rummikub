eaa7799fbb37134ba3635ce045f815b592533476
T31212Y <im.fa896@gmail.com>
2025-06-02 18:44:12 +0200

    changed access to token implementations, now they are accessed via token interface

--- src/main/scala/de/htwg/se/rummikub/util/TokenUtils.scala
+++ src/main/scala/de/htwg/se/rummikub/util/TokenUtils.scala
@@ -1,11 +1,15 @@
 package de.htwg.se.rummikub.util
 
-import de.htwg.se.rummikub.model._
+import de.htwg.se.rummikub.model.tokenComponent.TokenInterface
 
 object TokenUtils {
-    def tokensMatch(token1: Token, token2: Token): Boolean = (token1, token2) match {
-        case (NumToken(n1, c1), NumToken(n2, c2)) => n1 == n2 && c1 == c2
-        case (_: Joker, _: Joker) => true
-        case _ => false
+    def tokensMatch(token1: TokenInterface, token2: TokenInterface): Boolean = {
+        if(token1.isNumToken && token2.isNumToken) {
+            token1.number == token2.number && token1.color == token2.color
+        } else if (token1.isJoker && token2.isJoker) {
+            true
+        } else {
+            false
+        }
     }
 }
