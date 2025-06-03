13c25a8a5961b29ecac99d8cbe86cb859a02f220
anki1103 <akvollmi@gmx.de>
2025-06-03 14:01:59 +0200

    changed to protected var

--- src/main/scala/de/htwg/se/rummikub/model/TokenStructureComponent/TokenStructureInterface.scala
+++ src/main/scala/de/htwg/se/rummikub/model/TokenStructureComponent/TokenStructureInterface.scala
@@ -1,19 +1,17 @@
-package de.htwg.se.rummikub.model.TokenStructureComponent
+package de.htwg.se.rummikub.model.tokenStructureComponent
 
 import de.htwg.se.rummikub.model.tokenComponent.TokenInterface
 
 abstract class TokenStructureInterface(initial: List[TokenInterface]) {
 
-    var tokens: List[TokenInterface] = initial
+  protected var tokens: List[TokenInterface] = initial
 
-    def getTokens: List[TokenInterface] = tokens
-    def addToken(token: TokenInterface): Unit = tokens = tokens :+ token
-    def removeToken(token: TokenInterface): Unit = tokens = tokens.filterNot(_.equals(token))
+  def getTokens: List[TokenInterface] = tokens
+  def addToken(token: TokenInterface): Unit = tokens :+= token
+  def removeToken(token: TokenInterface): Unit = tokens = tokens.filterNot(_.equals(token))
 
-    override def toString: String = {
-        tokens.map(_.toString).mkString(" ")
-    }
+  override def toString: String = tokens.map(_.toString).mkString(" ")
 
-    def isValid: Boolean
-    def points: Int
+  def isValid: Boolean
+  def points: Int
 }
