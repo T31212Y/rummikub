58fedf26c82a981f2e87daa391034d8c935e7bb5
anki1103 <akvollmi@gmx.de>
2025-06-03 14:11:19 +0200

    changed PlayingField to PlayingFieldInterface

--- src/main/scala/de/htwg/se/rummikub/model/playingfieldComponent/PlayingField.scala
+++
@@ -1,6 +0,0 @@
-package de.htwg.se.rummikub.model.playingfieldComponent
-
-import de.htwg.se.rummikub.model.playingfieldComponent.playingFieldBaseImpl.{Board, Table, TokenStack}
-import de.htwg.se.rummikub.model.playerComponent.PlayerInterface
-
-case class PlayingFieldInterface(players: List[PlayerInterface], boards: List[Board], innerField: Table, stack: TokenStack)
