fa297efba950e27cc806bca335b1bf775b06e39b
anki1103 <akvollmi@gmx.de>
2025-06-03 13:34:52 +0200

    xed error and added packages

---
+++ src/main/scala/de/htwg/se/rummikub/model/playingfieldComponent/playingFieldBaseImpl/Table.scala
@@ -0,0 +1,74 @@
+package de.htwg.se.rummikub.model.playingfieldComponent.playingFieldBaseImpl
+
+import de.htwg.se.rummikub.util.TokenUtils.tokensMatch
+import de.htwg.se.rummikub.model.TokenStructureComponent.TokenStructureInterface
+
+case class Table(cntRows: Int, length: Int, tokensOnTable: List[List[TokenStructureInterface]] = List()) {
+
+    val emptyRow = "|" + (" " * length) + "|\n"
+
+    def deleteColorCodes(tableRow: String): String = {
+        tableRow.replaceAll("\u001B\\[[;\\d]*m", "")
+    }
+
+    def add (e: List[TokenStructureInterface]): Table = this.copy(tokensOnTable = this.tokensOnTable :+ e)
+
+    def remove(tokensToRemove: List[TokenStructureInterface]): Table = {
+        val removalBuffer = scala.collection.mutable.ListBuffer.from(tokensToRemove)
+
+        val updatedTokensOnTable = tokensOnTable.map { row =>
+            row.foldLeft(List.empty[TokenStructureInterface]) { (acc, token) =>
+                val indexOpt = removalBuffer.indexWhere(t => t.getTokens == token.getTokens)
+
+
+                if (indexOpt != -1) {
+                    removalBuffer.remove(indexOpt)
+                    acc
+                } else {
+                    acc :+ token
+                }
+            }
+        }.filter(_.nonEmpty)
+
+        this.copy(tokensOnTable = updatedTokensOnTable)
+    }
+
+    def getRow(index: Int): Option[List[TokenStructureInterface]] = {
+        tokensOnTable.lift(index)
+    }
+
+    def getGroup(index: Int): Option[List[TokenStructureInterface]] = {
+        tokensOnTable.lift(index)
+    }
+
+    def updateTokensOnTable(index: Int, newTokens: List[TokenStructureInterface]): Table = {
+        val updated =
+          if (index < tokensOnTable.length) {
+            tokensOnTable.updated(index, newTokens)
+          } else {
+            tokensOnTable ++ List.fill(index - tokensOnTable.length)(List.empty[TokenStructureInterface]) :+ newTokens
+          }
+        this.copy(tokensOnTable = updated)
+    }
+
+    override def toString: String = {
+        if (tokensOnTable.isEmpty) {
+            emptyRow.repeat(cntRows)
+        } else {
+            val formattedRows = tokensOnTable.map { row =>
+                val rawRow = row.map(_.toString).mkString(" ")
+                val rawRowLength = deleteColorCodes(rawRow).length
+                val padding = math.max(0, length - rawRowLength - 1)
+                val formattedRow = " " + rawRow + " " * padding
+                "|" + formattedRow + "|"
+            }.mkString("\n")
+
+            if (tokensOnTable.size < cntRows) {
+                val emptyRows = cntRows - tokensOnTable.size
+                formattedRows + "\n" + emptyRow.repeat(emptyRows)
+            } else {
+                formattedRows
+            }
+        }
+    }
+}
