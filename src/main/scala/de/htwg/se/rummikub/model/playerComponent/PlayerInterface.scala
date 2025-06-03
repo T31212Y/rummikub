b2d2e0a96074a8a7bb2a6f94636a1598a96828c7
anki1103 <akvollmi@gmx.de>
2025-06-03 12:59:10 +0200

    changed import in PlayerInterface

--- src/main/scala/de/htwg/se/rummikub/model/playerComponent/PlayerInterface.scala
+++ src/main/scala/de/htwg/se/rummikub/model/playerComponent/PlayerInterface.scala
@@ -2,11 +2,11 @@ package de.htwg.se.rummikub.model.playerComponent
 
 import de.htwg.se.rummikub.model.tokenComponent.TokenInterface
 
-import de.htwg.se.rummikub.model.TokenStructure
+import de.htwg.se.rummikub.model.TokenStructureComponent.TokenStructureInterface
 
 trait PlayerInterface {
   def validateFirstMove: Boolean
   def addToFirstMoveTokens(newTokens: List[TokenInterface]): PlayerInterface
   def deepCopy: PlayerInterface
-  def clusterTokens(tokens: List[TokenInterface]): List[TokenStructure]
+  def clusterTokens(tokens: List[TokenInterface]): List[TokenStructureInterface]
 }
