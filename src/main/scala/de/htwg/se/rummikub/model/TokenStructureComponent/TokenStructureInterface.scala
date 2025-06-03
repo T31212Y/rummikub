bef9a0a0a5c15c30c70ff582d08679d2eb41cc04
anki1103 <akvollmi@gmx.de>
2025-06-03 12:59:37 +0200

    created TokenstructureInterface

---
+++ src/main/scala/de/htwg/se/rummikub/model/TokenStructureComponent/TokenStructureInterface.scala
@@ -0,0 +1,19 @@
+package de.htwg.se.rummikub.model.TokenStructureComponent
+
+import de.htwg.se.rummikub.model.tokenComponent.TokenInterface
+
+abstract class TokenStructureInterface(initial: List[TokenInterface]) {
+
+    var tokens: List[TokenInterface] = initial
+
+    def getTokens: List[TokenInterface] = tokens
+    def addToken(token: TokenInterface): Unit = tokens = tokens :+ token
+    def removeToken(token: TokenInterface): Unit = tokens = tokens.filterNot(_.equals(token))
+
+    override def toString: String = {
+        tokens.map(_.toString).mkString(" ")
+    }
+
+    def isValid: Boolean
+    def points: Int
+}
