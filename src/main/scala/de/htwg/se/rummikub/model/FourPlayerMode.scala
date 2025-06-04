0439a365e468bbfbf8d5ce0ce483c4e05363a8e9
anki1103 <akvollmi@gmx.de>
2025-06-04 14:04:01 +0200

    changed import

--- src/main/scala/de/htwg/se/rummikub/model/FourPlayerMode.scala
+++ src/main/scala/de/htwg/se/rummikub/model/FourPlayerMode.scala
@@ -1,6 +1,6 @@
 package de.htwg.se.rummikub.model
 import de.htwg.se.rummikub.model.playingfieldComponent.PlayingFieldInterface
-import de.htwg.se.rummikub.model.playingfieldComponent.playingFieldBaseImpl.Table
+import de.htwg.se.rummikub.model.playingfieldComponent.TableInterface
 import de.htwg.se.rummikub.model.builderComponent.PlayingFieldBuilderInterface
 import de.htwg.se.rummikub.model.playerComponent.PlayerInterface
 import de.htwg.se.rummikub.model.playingfieldComponent.BoardInterface
