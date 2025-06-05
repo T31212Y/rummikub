0b743d8f520bdd1351ec53f7723ba5886a80e67a
anki1103 <akvollmi@gmx.de>
2025-06-05 11:58:05 +0200

    fixed PlayingFieldSpec

--- src/test/scala/de/htwg/se/rummikub/model/playingFieldComponent/playingFieldBaseImpl/PlayingFieldSpec.scala
+++ src/test/scala/de/htwg/se/rummikub/model/playingFieldComponent/playingFieldBaseImpl/PlayingFieldSpec.scala
@@ -2,34 +2,38 @@ package de.htwg.se.rummikub.model.playingFieldComponent.playingFieldBaseImpl
 
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
+import de.htwg.se.rummikub.model.gameModeComponent.gameModeBaseImpl.GameModeFactory
 
 class PlayingFieldSpec extends AnyWordSpec {
   "A PlayingField" should {
     "be created with the correct number of players" in {
-      val gameModeTry = gameModeComponent.GameModeFactory.createGameMode(2, List("Emilia", "Noah"))
+      val gameModeFactory = new GameModeFactory
+      val gameModeTry = gameModeFactory.createGameMode(2, List("Emilia", "Noah"))
       val gameMode = gameModeTry.get
-      val players = gameMode.createPlayers()
+      val players = gameMode.createPlayers
       val playingField = gameMode.createPlayingField(players).get
 
-      playingField.players.size should be(2)
+      playingField.getPlayers.size should be(2)
     }
 
     "have a board for each player" in {
-      val gameModeTry = gameModeComponent.GameModeFactory.createGameMode(2, List("Emilia", "Noah"))
+      val gameModeFactory = new GameModeFactory
+      val gameModeTry = gameModeFactory.createGameMode(2, List("Emilia", "Noah"))
       val gameMode = gameModeTry.get
-      val players = gameMode.createPlayers()
+      val players = gameMode.createPlayers
       val playingField = gameMode.createPlayingField(players).get
 
-      playingField.boards.size should be(2)
+      playingField.getBoards.size should be(2)
     }
 
     "have an inner field" in {
-      val gameModeTry = gameModeComponent.GameModeFactory.createGameMode(2, List("Emilia", "Noah"))
+      val gameModeFactory = new GameModeFactory
+      val gameModeTry = gameModeFactory.createGameMode(2, List("Emilia", "Noah"))
       val gameMode = gameModeTry.get
-      val players = gameMode.createPlayers()
+      val players = gameMode.createPlayers
       val playingField = gameMode.createPlayingField(players)
 
-      playingField.get.innerField should not be null
+      playingField.get.getInnerField should not be null
     }
   }
 }
