ceea643d8d7fb9d416a4c37c30dca9829bd05741
anki1103 <akvollmi@gmx.de>
2025-06-05 11:46:53 +0200

    fixed GameModeFactorySpec

--- src/test/scala/de/htwg/se/rummikub/model/gameModeComponent/gameModeBaseImpl/GameModeFactorySpec.scala
+++ src/test/scala/de/htwg/se/rummikub/model/gameModeComponent/gameModeBaseImpl/GameModeFactorySpec.scala
@@ -2,10 +2,11 @@ package de.htwg.se.rummikub.model.gameModeComponent.gameModeBaseImpl
 
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
-
+import de.htwg.se.rummikub.model.gameModeComponent.gameModeBaseImpl.GameModeFactory
+import de.htwg.se.rummikub.model.gameModeComponent.gameModeBaseImpl.TwoPlayerMode
+import de.htwg.se.rummikub.model.gameModeComponent.gameModeBaseImpl.ThreePlayerMode
+import de.htwg.se.rummikub.model.gameModeComponent.gameModeBaseImpl.FourPlayerMode
 import scala.util.{Success, Failure}
-import org.scalactic.Fail
-import gameModeComponent.FourPlayerMode
 
 class GameModeFactorySpec extends AnyWordSpec {
 
@@ -13,7 +14,8 @@ class GameModeFactorySpec extends AnyWordSpec {
 
     "create a TwoPlayerMode for 2 players" in {
       val playerNames = List("Alice", "Bob")
-      val mode = GameModeFactory.createGameMode(2, playerNames)
+      val gameModeFactory = new GameModeFactory
+      val mode = gameModeFactory.createGameMode(2, playerNames)
 
       mode shouldBe a [Success[TwoPlayerMode]]
       mode.get shouldBe a [TwoPlayerMode]
@@ -22,7 +24,8 @@ class GameModeFactorySpec extends AnyWordSpec {
 
     "create a ThreePlayerMode for 3 players" in {
       val playerNames = List("Alice", "Bob", "Charlie")
-      val mode = GameModeFactory.createGameMode(3, playerNames)
+      val gameModeFactory = new GameModeFactory
+      val mode = gameModeFactory.createGameMode(3, playerNames)
 
       mode shouldBe a [Success[ThreePlayerMode]]
       mode.get shouldBe a [ThreePlayerMode]
@@ -31,7 +34,8 @@ class GameModeFactorySpec extends AnyWordSpec {
 
     "create a FourPlayerMode for 4 players" in {
       val playerNames = List("Alice", "Bob", "Charlie", "Diana")
-      val mode = GameModeFactory.createGameMode(4, playerNames)
+      val gameModeFactory = new GameModeFactory
+      val mode = gameModeFactory.createGameMode(4, playerNames)
 
       mode shouldBe a [Success[FourPlayerMode]]
       mode.get shouldBe a [FourPlayerMode]
@@ -40,7 +44,8 @@ class GameModeFactorySpec extends AnyWordSpec {
 
     "throw a MatchError for unsupported number of players" in {
       val playerNames = List("Alice")
-      val mode = GameModeFactory.createGameMode(1, playerNames)
+      val gameModeFactory = new GameModeFactory
+      val mode = gameModeFactory.createGameMode(1, playerNames)
 
       mode shouldBe a [Failure[Nothing]]
       mode.failed.get shouldBe an [IllegalArgumentException]
