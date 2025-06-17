2bbc7fc542a1afb57af1c57075037c0ec03c985a
T31212Y <im.fa896@gmail.com>
2025-06-17 22:13:08 +0200

    added @Inject() and import

--- src/main/scala/de/htwg/se/rummikub/model/gameModeComponent/gameModeBaseImpl/GameModeFactory.scala
+++ src/main/scala/de/htwg/se/rummikub/model/gameModeComponent/gameModeBaseImpl/GameModeFactory.scala
@@ -3,7 +3,9 @@ package de.htwg.se.rummikub.model.gameModeComponent.gameModeBaseImpl
 import scala.util.{Try, Success, Failure}
 import de.htwg.se.rummikub.model.gameModeComponent.{GameModeTemplate, GameModeFactoryInterface}
 
-class GameModeFactory extends GameModeFactoryInterface {
+import com.google.inject.Inject
+
+class GameModeFactory @Inject() extends GameModeFactoryInterface {
   override def createGameMode(amtPlayers: Int, playerNames: List[String]): Try[GameModeTemplate] = amtPlayers match {
     case 2 => Success(TwoPlayerMode(playerNames))
     case 3 => Success(ThreePlayerMode(playerNames))
