dcf77c6eccbe8d843b05f2042207e668496c3a06
T31212Y <im.fa896@gmail.com>
2025-06-18 22:35:43 +0200

    added new methods isJoker and isNumToken

--- src/main/scala/de/htwg/se/rummikub/model/tokenComponent/tokenBaseImpl/Joker.scala
+++ src/main/scala/de/htwg/se/rummikub/model/tokenComponent/tokenBaseImpl/Joker.scala
@@ -5,6 +5,9 @@ import de.htwg.se.rummikub.model.tokenComponent.{TokenInterface, Color}
 case class Joker(color: Color) extends TokenInterface {
   override def getNumber: Option[Int] = None
   override def getColor: Color = color
+
+  override def isJoker: Boolean = true
+  override def isNumToken: Boolean = false
   
   override def toString: String = s" ${color.toString}J${color.reset}"
 }
