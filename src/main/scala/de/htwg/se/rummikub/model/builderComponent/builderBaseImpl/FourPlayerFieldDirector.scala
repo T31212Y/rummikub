8ee73fc12a3b517a23d334596a4d89c4b67606ee
T31212Y <im.fa896@gmail.com>
2025-06-18 18:51:47 +0200

    update FieldDirector classes to use TokenStackFactory for stack creation

--- src/main/scala/de/htwg/se/rummikub/model/builderComponent/builderBaseImpl/FourPlayerFieldDirector.scala
+++ src/main/scala/de/htwg/se/rummikub/model/builderComponent/builderBaseImpl/FourPlayerFieldDirector.scala
@@ -2,18 +2,20 @@ package de.htwg.se.rummikub.model.builderComponent.builderBaseImpl
 
 import de.htwg.se.rummikub.model.playerComponent.PlayerInterface
 
-import de.htwg.se.rummikub.model.playingFieldComponent.playingFieldBaseImpl.{Board, Table, TokenStack}
+import de.htwg.se.rummikub.model.playingFieldComponent.playingFieldBaseImpl.{Board, Table}
 import de.htwg.se.rummikub.model.playingFieldComponent.PlayingFieldInterface
 import de.htwg.se.rummikub.model.builderComponent.PlayingFieldBuilderInterface
 import de.htwg.se.rummikub.model.builderComponent.FieldDirectorInterface
 
-class FourPlayerFieldDirector(builder: PlayingFieldBuilderInterface) extends FieldDirectorInterface {
+import de.htwg.se.rummikub.model.playingFieldComponent.TokenStackFactoryInterface
+
+class FourPlayerFieldDirector(builder: PlayingFieldBuilderInterface, tokenStackFactory: TokenStackFactoryInterface) extends FieldDirectorInterface {
   override def construct(players: List[PlayerInterface]): PlayingFieldInterface = {
     val boardP13 = new Board(cntEdgeSpaces, cntTokens, 4, 2, "up")
     val boardP24 = new Board(cntEdgeSpaces, cntTokens, 4, 2, "down")
 
     val innerField = new Table(cntRows - 4, boardP13.size(boardP13.wrapBoardRowDouble(boardP13.boardELRP12_1, boardP13.boardELRP34_1)) - 2)
-    val stack = TokenStack.apply()
+    val stack = tokenStackFactory.createShuffledStack
 
     builder.setPlayers(players)
       .setBoards(List(boardP13, boardP24))
--- src/main/scala/de/htwg/se/rummikub/model/builderComponent/builderBaseImpl/TwoPlayerFieldDirector.scala
+++ src/main/scala/de/htwg/se/rummikub/model/builderComponent/builderBaseImpl/TwoPlayerFieldDirector.scala
@@ -2,18 +2,20 @@ package de.htwg.se.rummikub.model.builderComponent.builderBaseImpl
 
 import de.htwg.se.rummikub.model.playerComponent.PlayerInterface
 
-import de.htwg.se.rummikub.model.playingFieldComponent.playingFieldBaseImpl.{Board, Table, TokenStack}
+import de.htwg.se.rummikub.model.playingFieldComponent.playingFieldBaseImpl.{Board, Table}
 import de.htwg.se.rummikub.model.playingFieldComponent.PlayingFieldInterface
 import de.htwg.se.rummikub.model.builderComponent.PlayingFieldBuilderInterface
 import de.htwg.se.rummikub.model.builderComponent.FieldDirectorInterface
 
-class TwoPlayerFieldDirector(builder: PlayingFieldBuilderInterface) extends FieldDirectorInterface {
+import de.htwg.se.rummikub.model.playingFieldComponent.TokenStackFactoryInterface
+
+class TwoPlayerFieldDirector(builder: PlayingFieldBuilderInterface, tokenStackFactory: TokenStackFactoryInterface) extends FieldDirectorInterface {
   override def construct(players: List[PlayerInterface]): PlayingFieldInterface = {
     val boardP1 = new Board(cntEdgeSpaces, cntTokens, 2, 1, "up")
     val boardP2 = new Board(cntEdgeSpaces, cntTokens, 2, 1, "down")
 
     val innerField = new Table(cntRows - 4, boardP1.size(boardP1.wrapBoardRowSingle(boardP1.boardELRP12_1)) - 2)
-    val stack = TokenStack.apply()
+    val stack = tokenStackFactory.createShuffledStack
 
     builder.setPlayers(players)
       .setBoards(List(boardP1, boardP2))
