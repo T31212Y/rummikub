e0dd344424c1862c5c092489ae668d4ec1d9090f
T31212Y <im.fa896@gmail.com>
2025-06-18 19:52:16 +0200

    updated TableFactoryInterface and StandardTableFactory to include tokensOnTable parameter in createTable method

--- src/main/scala/de/htwg/se/rummikub/model/playingFieldComponent/TableFactoryInterface.scala
+++ src/main/scala/de/htwg/se/rummikub/model/playingFieldComponent/TableFactoryInterface.scala
@@ -1,5 +1,7 @@
 package de.htwg.se.rummikub.model.playingFieldComponent
 
+import de.htwg.se.rummikub.model.tokenComponent.TokenInterface
+
 trait TableFactoryInterface {
-    def createTable(cntRows: Int, length: Int): TableInterface
+    def createTable(cntRows: Int, length: Int, tokensOnTable: List[List[TokenInterface]]): TableInterface
 }
--- src/main/scala/de/htwg/se/rummikub/model/playingFieldComponent/playingFieldBaseImpl/StandardTableFactory.scala
+++ src/main/scala/de/htwg/se/rummikub/model/playingFieldComponent/playingFieldBaseImpl/StandardTableFactory.scala
@@ -1,7 +1,8 @@
 package de.htwg.se.rummikub.model.playingFieldComponent.playingFieldBaseImpl
 
 import de.htwg.se.rummikub.model.playingFieldComponent.{TableFactoryInterface, TableInterface}
+import de.htwg.se.rummikub.model.tokenComponent.TokenInterface
 
 class StandardTableFactory extends TableFactoryInterface {
-  override def createTable(cntRows: Int, length: Int): TableInterface = new Table(cntRows, length)
+  override def createTable(cntRows: Int, length: Int, tokensOnTable: List[List[TokenInterface]]): TableInterface = new Table(cntRows, length, tokensOnTable)
 }
