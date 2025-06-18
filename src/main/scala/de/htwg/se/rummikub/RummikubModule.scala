5ea85d17153aea208dabd4bed587b43cd4f24de9
T31212Y <im.fa896@gmail.com>
2025-06-18 19:26:38 +0200

    added binding to tableFactory and deleted binding to table class

--- src/main/scala/de/htwg/se/rummikub/RummikubModule.scala
+++ src/main/scala/de/htwg/se/rummikub/RummikubModule.scala
@@ -15,20 +15,20 @@ import de.htwg.se.rummikub.model.gameModeComponent.gameModeBaseImpl.{GameModeFac
 import de.htwg.se.rummikub.model.playerComponent.PlayerInterface
 import de.htwg.se.rummikub.model.playerComponent.playerBaseImpl.Player
 
-import de.htwg.se.rummikub.model.playingFieldComponent.{PlayingFieldInterface, BoardInterface, TableInterface, TokenStackInterface}
-import de.htwg.se.rummikub.model.playingFieldComponent.playingFieldBaseImpl.{PlayingField, Board, Table, TokenStack}
+import de.htwg.se.rummikub.model.playingFieldComponent.{PlayingFieldInterface, BoardInterface, TableInterface}
+import de.htwg.se.rummikub.model.playingFieldComponent.playingFieldBaseImpl.{PlayingField, Board, Table}
 
-import de.htwg.se.rummikub.model.tokenComponent.{TokenInterface, TokenFactoryInterface}
-import de.htwg.se.rummikub.model.tokenComponent.tokenBaseImpl.{Joker, NumToken, StandardTokenFactory}
-
-import de.htwg.se.rummikub.model.tokenStructureComponent.TokenStructureInterface
-import de.htwg.se.rummikub.model.tokenStructureComponent.tokenStructureBaseImpl.{Row, Group}
+import de.htwg.se.rummikub.model.tokenComponent.TokenFactoryInterface
+import de.htwg.se.rummikub.model.tokenComponent.tokenBaseImpl.StandardTokenFactory
 
 import de.htwg.se.rummikub.model.tokenStructureComponent.TokenStructureFactoryInterface
 import de.htwg.se.rummikub.model.tokenStructureComponent.tokenStructureBaseImpl.StandardTokenStructureFactory
 
 import de.htwg.se.rummikub.model.playingFieldComponent.TokenStackFactoryInterface
 import de.htwg.se.rummikub.model.playingFieldComponent.playingFieldBaseImpl.StandardTokenStackFactory
+
+import de.htwg.se.rummikub.model.playingFieldComponent.TableFactoryInterface
+import de.htwg.se.rummikub.model.playingFieldComponent.playingFieldBaseImpl.StandardTableFactory
 
 class RummikubModule extends AbstractModule with ScalaModule {
   override def configure(): Unit = {
@@ -48,10 +48,10 @@ class RummikubModule extends AbstractModule with ScalaModule {
     bind[PlayerInterface].to[Player]
 
     bind[PlayingFieldInterface].to[PlayingField]
-    bind[BoardInterface].to[Board]
-    bind[TableInterface].to[Table]*/
+    bind[BoardInterface].to[Board]*/
     bind[TokenFactoryInterface].to[StandardTokenFactory]
     bind[TokenStructureFactoryInterface].to[StandardTokenStructureFactory]
     bind[TokenStackFactoryInterface].to[StandardTokenStackFactory]
+    bind[TableFactoryInterface].to[StandardTableFactory]
   }
 }
