022e063c4dd8e403313790a6a5d6be3e14c62270
anki1103 <akvollmi@gmx.de>
2025-06-18 11:54:34 +0200

    added given

--- src/main/scala/de/htwg/se/rummikub/Rummikub.scala
+++ src/main/scala/de/htwg/se/rummikub/Rummikub.scala
@@ -1,6 +1,7 @@
 package de.htwg.se.rummikub
 
 import de.htwg.se.rummikub.controller.controllerComponent.controllerBaseImpl.Controller
+import de.htwg.se.rummikub.controller.controllerComponent.ControllerInterface
 import de.htwg.se.rummikub.aview._
 import de.htwg.se.rummikub.model.gameModeComponent.gameModeBaseImpl.GameModeFactory
 
@@ -10,20 +11,22 @@ import de.htwg.se.rummikub.aview.gui.Gui
 object Rummikub {
   val gameModeFactory = new GameModeFactory
   val controller = new Controller(gameModeFactory.createGameMode(2, List("Emilia", "Noah")).get, gameModeFactory)
-  val tui: GameView = new Tui(controller)
-  val gui: GameView = new Gui(controller)
+
+  given ControllerInterface = controller
+
+  val tui: GameView = new Tui
+  val gui: GameView = new Gui
 
   def main(args: Array[String]): Unit = {
     var input = ""
-
     println(tui.showWelcome.mkString("\n") + "\n")
     controller.setupNewGame(2, List("Emilia", "Noah"))
 
     while (input != "quit") {
-        println(tui.showHelp)
-        println("Please enter a command:")
-        input = readLine()
-        tui.inputCommands(input)
+      println(tui.showHelp)
+      println("Please enter a command:")
+      input = readLine()
+      tui.inputCommands(input)
     }
   }
 }
