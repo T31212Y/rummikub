031f82af1b1f8fac5767c89d3e0cc5473b59d01a
T31212Y <im.fa896@gmail.com>
2025-06-19 13:57:26 +0200

    added imports and given metric for tableFactory

--- src/main/scala/de/htwg/se/rummikub/RummikubDependencyModule.scala
+++ src/main/scala/de/htwg/se/rummikub/RummikubDependencyModule.scala
@@ -15,10 +15,14 @@ import de.htwg.se.rummikub.model.tokenStructureComponent.tokenStructureBaseImpl.
 import de.htwg.se.rummikub.model.playingFieldComponent.TokenStackFactoryInterface
 import de.htwg.se.rummikub.model.playingFieldComponent.playingFieldBaseImpl.StandardTokenStackFactory
 
+import de.htwg.se.rummikub.model.playingFieldComponent.TableFactoryInterface
+import de.htwg.se.rummikub.model.playingFieldComponent.playingFieldBaseImpl.StandardTableFactory
+
 object RummikubDependencyModule {
     given tokenFactory: TokenFactoryInterface = new StandardTokenFactory
     given tokenStackFactory: TokenStackFactoryInterface = new StandardTokenStackFactory
     given gameModeFactory: GameModeFactoryInterface = new GameModeFactory
     given tokenStructureFactory: TokenStructureFactoryInterface = new StandardTokenStructureFactory
+    given tableFactory: TableFactoryInterface = new StandardTableFactory
     given controller: ControllerInterface = new Controller
 }
