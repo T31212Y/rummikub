5d5a0476fa5ccf178249e3e96c623bfae2cae970
T31212Y <im.fa896@gmail.com>
2025-06-19 12:01:43 +0200

    deleted val in using call

--- src/main/scala/de/htwg/se/rummikub/aview/gui/Gui.scala
+++ src/main/scala/de/htwg/se/rummikub/aview/gui/Gui.scala
@@ -9,7 +9,7 @@ import javax.swing.BorderFactory
 import javax.swing.border.TitledBorder
 import javax.swing.ImageIcon
 
-class Gui(using val controller: ControllerInterface) extends Frame with Reactor with GameView {
+class Gui(using controller: ControllerInterface) extends Frame with Reactor with GameView {
 
   listenTo(controller)
   reactions += {
