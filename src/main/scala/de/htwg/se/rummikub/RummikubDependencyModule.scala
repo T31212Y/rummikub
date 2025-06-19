340ffa2faa7c7468dc3a5ffa3c0a776225cd928a
T31212Y <im.fa896@gmail.com>
2025-06-19 12:19:09 +0200

    added imports and given metric for tokenFactory

--- src/main/scala/de/htwg/se/rummikub/RummikubDependencyModule.scala
+++ src/main/scala/de/htwg/se/rummikub/RummikubDependencyModule.scala
@@ -6,7 +6,11 @@ import de.htwg.se.rummikub.model.gameModeComponent.gameModeBaseImpl.GameModeFact
 import de.htwg.se.rummikub.controller.controllerComponent.ControllerInterface
 import de.htwg.se.rummikub.controller.controllerComponent.controllerBaseImpl.Controller
 
+import de.htwg.se.rummikub.model.tokenComponent.TokenFactoryInterface
+import de.htwg.se.rummikub.model.tokenComponent.tokenBaseImpl.StandardTokenFactory
+
 object RummikubDependencyModule {
     given gameModeFactory: GameModeFactoryInterface = new GameModeFactory
+    given tokenFactory: TokenFactoryInterface = new StandardTokenFactory
     given controller: ControllerInterface = new Controller
 }
