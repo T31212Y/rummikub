ba4d99155f20385ec3cf05a81fbcc8d088fa9f3d
T31212Y <im.fa896@gmail.com>
2025-06-19 13:57:45 +0200

    created TableFactoryInterface with base implementation

---
+++ src/main/scala/de/htwg/se/rummikub/model/playingFieldComponent/TableFactoryInterface.scala
@@ -0,0 +1,7 @@
+package de.htwg.se.rummikub.model.playingFieldComponent
+
+import de.htwg.se.rummikub.model.tokenComponent.TokenInterface
+
+trait TableFactoryInterface {
+  def createTable(cntRows: Int, length: Int, tokensOnTable: List[List[TokenInterface]]): TableInterface
+}
