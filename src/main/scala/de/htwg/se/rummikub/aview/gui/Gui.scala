a8488d6a9a10beab71883f01485bea6b3e397a5c
anki1103 <akvollmi@gmx.de>
2025-06-21 12:24:35 +0200

    splitted in group and rows and selected token is leaving the table

--- src/main/scala/de/htwg/se/rummikub/aview/gui/Gui.scala
+++ src/main/scala/de/htwg/se/rummikub/aview/gui/Gui.scala
@@ -294,7 +294,7 @@ class Gui(controller: ControllerInterface) extends Frame with Reactor with GameV
     case ButtonClicked(`redoButton`) => controller.redo
 
     case ButtonClicked(`putInStorageButton`) =>
-      val displayList = controller.getDisplayStringForTokensWithIndex
+      val displayList = controller.getFormattedTokensOnTableWithLabels
       val input = Dialog.showInput(
         parent = null,
         message = s"Tokens on Table:\n$displayList\n\nEnter the index of the token to put in Storage:",
@@ -309,6 +309,7 @@ class Gui(controller: ControllerInterface) extends Frame with Reactor with GameV
             controller.putTokenInStorage(tokenId) match {
               case Some(newState) =>
                 controller.setStateInternal(newState)
+                update
               case None =>
                 Dialog.showMessage(null, "Ungültiger Token-Index!")
             }
@@ -316,226 +317,225 @@ class Gui(controller: ControllerInterface) extends Frame with Reactor with GameV
             case _: NumberFormatException =>
               Dialog.showMessage(null, "Invalid index format!")
           }
-
         case Some(_) =>
           Dialog.showMessage(null, "No index entered.")
 
         case None =>
       }
-  }
+    }
   
-  def updatePlayerBoardTitle(state: GameStateInterface): Unit = {
-    val titledBorder = playerBoardPanel.border.asInstanceOf[TitledBorder]
-    titledBorder.setTitle(s"${state.currentPlayer.getName} ")
+    def updatePlayerBoardTitle(state: GameStateInterface): Unit = {
+      val titledBorder = playerBoardPanel.border.asInstanceOf[TitledBorder]
+      titledBorder.setTitle(s"${state.currentPlayer.getName} ")
 
-    playerBoardPanel.repaint()
-    playerBoardPanel.revalidate()
-  }
-
-  def updatePlayerTokens: Unit = {
-    playerBoardPanel.contents.clear()
-
-    val currentPlayer = controller.getState.currentPlayer
-
-    for ((token, index) <- currentPlayer.getTokens.zipWithIndex) {
-      val panel = TokenPanel(token, controller)
-      playerBoardPanel.contents += panel
+      playerBoardPanel.repaint()
+      playerBoardPanel.revalidate()
     }
 
-    playerBoardPanel.revalidate()
-    playerBoardPanel.repaint()
-  }
+    def updatePlayerTokens: Unit = {
+      playerBoardPanel.contents.clear()
 
-  def updateStoragePanel(): Unit = {
-    storagePanel.contents.clear()
-    val storageTokens = controller.getState.getStorageTokens
+      val currentPlayer = controller.getState.currentPlayer
 
-    for (token <- storageTokens) {
-      val tokenObj: de.htwg.se.rummikub.model.tokenComponent.TokenInterface = controller.getTokenFromString(token)
-      storagePanel.contents += TokenPanel(tokenObj, controller)
-    }
-
-    storagePanel.revalidate()
-    storagePanel.repaint()
-  }
-
-  def updateTable: Unit = {
-    tablePanel.contents.clear()
-
-    val tableTokens = controller.getState.getTable.getTokensOnTable
-
-    val groupsPerRow = 5
-    val rows = tableTokens.grouped(groupsPerRow).toSeq
-
-    val rowsToShow = rows.take(2)
-
-    for ((rowGroups, rowIndex) <- rowsToShow.zipWithIndex) {
-      val rowPanel = new FlowPanel(FlowPanel.Alignment.Left)() {
-        opaque = false
-        hGap = 15
-        vGap = 10
-        tablePanel.preferredSize = new Dimension(1000, 250)
+      for ((token, index) <- currentPlayer.getTokens.zipWithIndex) {
+        val panel = TokenPanel(token, controller)
+        playerBoardPanel.contents += panel
       }
 
-      for ((tokenGroup, groupIndex) <- rowGroups.zipWithIndex) {
-        val groupPanel = new FlowPanel(FlowPanel.Alignment.Center)() {
+      playerBoardPanel.revalidate()
+      playerBoardPanel.repaint()
+    }
+
+    def updateStoragePanel(): Unit = {
+      storagePanel.contents.clear()
+      val storageTokens = controller.getState.getStorageTokens
+
+      for (token <- storageTokens) {
+        val tokenObj: de.htwg.se.rummikub.model.tokenComponent.TokenInterface = controller.getTokenFromString(token)
+        storagePanel.contents += TokenPanel(tokenObj, controller)
+      }
+
+      storagePanel.revalidate()
+      storagePanel.repaint()
+    }
+
+    def updateTable: Unit = {
+      tablePanel.contents.clear()
+
+      val tableTokens = controller.getState.getTable.getTokensOnTable
+
+      val groupsPerRow = 5
+      val rows = tableTokens.grouped(groupsPerRow).toSeq
+
+      val rowsToShow = rows.take(2)
+
+      for ((rowGroups, rowIndex) <- rowsToShow.zipWithIndex) {
+        val rowPanel = new FlowPanel(FlowPanel.Alignment.Left)() {
           opaque = false
-          hGap = 4
-          vGap = 2
-
-          val borderColor = java.awt.Color.WHITE
-          val borderFont = new Font("Arial", java.awt.Font.BOLD, 12)
-
-          border = BorderFactory.createTitledBorder(
-            BorderFactory.createLineBorder(borderColor),
-            s"index ${rowIndex * groupsPerRow + groupIndex}",
-            javax.swing.border.TitledBorder.CENTER,
-            javax.swing.border.TitledBorder.BOTTOM,
-            borderFont,
-            borderColor
-          )
+          hGap = 15
+          vGap = 10
+          tablePanel.preferredSize = new Dimension(1000, 250)
         }
 
-        tokenGroup.foreach { token =>
-          groupPanel.contents += TokenPanel(token, controller)
-        }
+        for ((tokenGroup, groupIndex) <- rowGroups.zipWithIndex) {
+          val groupPanel = new FlowPanel(FlowPanel.Alignment.Center)() {
+            opaque = false
+            hGap = 4
+            vGap = 2
 
-        rowPanel.contents += groupPanel
-      }
+            val borderColor = java.awt.Color.WHITE
+            val borderFont = new Font("Arial", java.awt.Font.BOLD, 12)
 
-      tablePanel.contents += rowPanel
-      tablePanel.contents += Swing.VStrut(10)
-    }
-
-    tablePanel.revalidate()
-    tablePanel.repaint()
-  }
-
-  def update: Unit = {
-    val state = controller.getState
-
-    updatePlayerTokens
-    updateTable
-    updateStoragePanel()
-
-    drawButton.enabled = !state.currentStack.isEmpty
-
-    tokenStackSizeLabel.text = s"Remaining tokens in stack: ${controller.getState.currentStack.size}"
-  }
-
-  override def playGame: Unit = {
-    controller.startGame
-    updatePlayerBoardTitle(controller.getState)
-    update
-    nextTurn
-  }
-
-  def nextTurn: Unit = {
-    if (controller.winGame || controller.getGameEnded) {
-      return
-    }
-
-    if (controller.getState.currentStack.isEmpty && controller.getState.getFinalRoundsLeft.isEmpty) {
-      val playersRemaining = controller.getState.getPlayers.length
-      controller.setStateInternal(controller.getState.updated(newPlayers = controller.getState.getPlayers, newStack = controller.getState.currentStack, newFinalRoundsLeft = Some(playersRemaining)))
-      stateLabel.text = s"No tokens left in stack, final round begins! You have $playersRemaining turns left to play."
-
-      finalRoundsLabel.visible = true
-    }
-
-    controller.getState.getFinalRoundsLeft match {
-      case Some(0) =>
-        finalRoundsLabel.text = "Final rounds left: 0"
-        controller.setGameEnded(true)
-        val winnerMessage = controller.endGame
-
-        Dialog.showMessage(contents.head, winnerMessage, title = "Game is over!")
-        stateLabel.text = "Game is over!"
-        return
-
-      case Some(n) =>
-        finalRoundsLabel.text = s"Final rounds left: ${controller.getState.getFinalRoundsLeft.getOrElse("")}"
-        controller.setStateInternal(controller.getState.updated(newPlayers = controller.getState.getPlayers, newStack = controller.getState.currentStack, newFinalRoundsLeft = Some(n - 1)))
-
-      case None =>
-    }
-
-    val currentPlayer = controller.getState.currentPlayer
-    val stack = controller.getState.currentStack
-    controller.setPlayingField(controller.getGameMode.updatePlayingField(controller.getPlayingField))
-    stateLabel.text = currentPlayer.getName + ", it's your turn!"
-
-    controller.beginTurn(currentPlayer)
-
-    update
-  }
-
-  def createButtonRow(button1: Button, button2: Button, width: Int = 130, height: Int = 30): FlowPanel = {
-    val preferredSize = new Dimension(width, height)
-    button1.preferredSize = preferredSize
-    button2.preferredSize = preferredSize
-
-    new FlowPanel(FlowPanel.Alignment.Left)(button1, button2) {
-      hGap = 5
-      vGap = 0
-      background = new java.awt.Color(0, 41, 159)
-      border = Swing.EmptyBorder(0, 0, 0, 0)
-      maximumSize = new Dimension(300, height + 5)
-    }
-  }
-
-  override def createNewGame: Unit = {
-    stateLabel.text = "Creating a new game..."
-    val input = Dialog.showInput(parent = null, message = askAmountOfPlayers, title = "Create a new game", initial = "")
-
-      input match {
-        case Some(amtStr) =>
-          val amt = amtStr.trim.toInt
-          val nameInput = Dialog.showInput(parent = null, message = askPlayerNames, title = "Create a new game", initial = "")
-
-          nameInput match {
-            case Some(namesStr) =>
-                val names = namesStr.split(",").map(_.trim).toList
-                controller.setupNewGame(amt, names)
-                stateLabel.text = "New game created!"
-            case None =>
+            border = BorderFactory.createTitledBorder(
+              BorderFactory.createLineBorder(borderColor),
+              s"index ${rowIndex * groupsPerRow + groupIndex}",
+              javax.swing.border.TitledBorder.CENTER,
+              javax.swing.border.TitledBorder.BOTTOM,
+              borderFont,
+              borderColor
+            )
           }
 
-        case None =>
-          stateLabel.text = "No input provided."
+          tokenGroup.foreach { token =>
+            groupPanel.contents += TokenPanel(token, controller)
+          }
+
+          rowPanel.contents += groupPanel
+        }
+
+        tablePanel.contents += rowPanel
+        tablePanel.contents += Swing.VStrut(10)
       }
-  }
 
-  def showManualImages(): Unit = {
-    val img1 = new Label {
-      icon = new ImageIcon(getClass.getResource("/gameManualP1.jpg"))
+      tablePanel.revalidate()
+      tablePanel.repaint()
     }
 
-    val img2 = new Label {
-      icon = new ImageIcon(getClass.getResource("/gameManualP2.jpg"))
+    def update: Unit = {
+      val state = controller.getState
+
+      updatePlayerTokens
+      updateTable
+      updateStoragePanel()
+
+      drawButton.enabled = !state.currentStack.isEmpty
+
+      tokenStackSizeLabel.text = s"Remaining tokens in stack: ${controller.getState.currentStack.size}"
     }
 
-    val panel = new BoxPanel(Orientation.Vertical)
-    val imgLabels = Seq(img1, img2)
-
-    imgLabels.foreach { label =>
-      panel.contents += label
-      panel.contents += Swing.VStrut(10)
+    override def playGame: Unit = {
+      controller.startGame
+      updatePlayerBoardTitle(controller.getState)
+      update
+      nextTurn
     }
 
-    val scrollPane = new ScrollPane(panel) {
-      preferredSize = new Dimension(1200, 700)
+    def nextTurn: Unit = {
+      if (controller.winGame || controller.getGameEnded) {
+        return
+      }
+
+      if (controller.getState.currentStack.isEmpty && controller.getState.getFinalRoundsLeft.isEmpty) {
+        val playersRemaining = controller.getState.getPlayers.length
+        controller.setStateInternal(controller.getState.updated(newPlayers = controller.getState.getPlayers, newStack = controller.getState.currentStack, newFinalRoundsLeft = Some(playersRemaining)))
+        stateLabel.text = s"No tokens left in stack, final round begins! You have $playersRemaining turns left to play."
+
+        finalRoundsLabel.visible = true
+      }
+
+      controller.getState.getFinalRoundsLeft match {
+        case Some(0) =>
+          finalRoundsLabel.text = "Final rounds left: 0"
+          controller.setGameEnded(true)
+          val winnerMessage = controller.endGame
+
+          Dialog.showMessage(contents.head, winnerMessage, title = "Game is over!")
+          stateLabel.text = "Game is over!"
+          return
+
+        case Some(n) =>
+          finalRoundsLabel.text = s"Final rounds left: ${controller.getState.getFinalRoundsLeft.getOrElse("")}"
+          controller.setStateInternal(controller.getState.updated(newPlayers = controller.getState.getPlayers, newStack = controller.getState.currentStack, newFinalRoundsLeft = Some(n - 1)))
+
+        case None =>
+      }
+
+      val currentPlayer = controller.getState.currentPlayer
+      val stack = controller.getState.currentStack
+      controller.setPlayingField(controller.getGameMode.updatePlayingField(controller.getPlayingField))
+      stateLabel.text = currentPlayer.getName + ", it's your turn!"
+
+      controller.beginTurn(currentPlayer)
+
+      update
     }
 
-    val helpFrame = new Frame {
-      title = "Game Manual"
-      contents = scrollPane
-      size = new Dimension(1220, 720)
-      centerOnScreen()
+    def createButtonRow(button1: Button, button2: Button, width: Int = 130, height: Int = 30): FlowPanel = {
+      val preferredSize = new Dimension(width, height)
+      button1.preferredSize = preferredSize
+      button2.preferredSize = preferredSize
+
+      new FlowPanel(FlowPanel.Alignment.Left)(button1, button2) {
+        hGap = 5
+        vGap = 0
+        background = new java.awt.Color(0, 41, 159)
+        border = Swing.EmptyBorder(0, 0, 0, 0)
+        maximumSize = new Dimension(300, height + 5)
+      }
     }
 
-    helpFrame.visible = true
-  }
+    override def createNewGame: Unit = {
+      stateLabel.text = "Creating a new game..."
+      val input = Dialog.showInput(parent = null, message = askAmountOfPlayers, title = "Create a new game", initial = "")
 
-  visible = true
+        input match {
+          case Some(amtStr) =>
+            val amt = amtStr.trim.toInt
+            val nameInput = Dialog.showInput(parent = null, message = askPlayerNames, title = "Create a new game", initial = "")
+
+            nameInput match {
+              case Some(namesStr) =>
+                  val names = namesStr.split(",").map(_.trim).toList
+                  controller.setupNewGame(amt, names)
+                  stateLabel.text = "New game created!"
+              case None =>
+            }
+
+          case None =>
+            stateLabel.text = "No input provided."
+        }
+    }
+
+    def showManualImages(): Unit = {
+      val img1 = new Label {
+        icon = new ImageIcon(getClass.getResource("/gameManualP1.jpg"))
+      }
+
+      val img2 = new Label {
+        icon = new ImageIcon(getClass.getResource("/gameManualP2.jpg"))
+      }
+
+      val panel = new BoxPanel(Orientation.Vertical)
+      val imgLabels = Seq(img1, img2)
+
+      imgLabels.foreach { label =>
+        panel.contents += label
+        panel.contents += Swing.VStrut(10)
+      }
+
+      val scrollPane = new ScrollPane(panel) {
+        preferredSize = new Dimension(1200, 700)
+      }
+
+      val helpFrame = new Frame {
+        title = "Game Manual"
+        contents = scrollPane
+        size = new Dimension(1220, 720)
+        centerOnScreen()
+      }
+
+      helpFrame.visible = true
+    }
+
+    visible = true
 }
