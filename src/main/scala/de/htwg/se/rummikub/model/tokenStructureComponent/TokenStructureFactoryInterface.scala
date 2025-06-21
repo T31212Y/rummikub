24d5df0c9309965425c33275e5f8f903f6ff6786
anki1103 <akvollmi@gmx.de>
2025-06-21 16:04:45 +0200

    now you cant pass if the table is not valid or/and the storage has tokens left

--- src/main/scala/de/htwg/se/rummikub/model/tokenStructureComponent/TokenStructureFactoryInterface.scala
+++ src/main/scala/de/htwg/se/rummikub/model/tokenStructureComponent/TokenStructureFactoryInterface.scala
@@ -5,6 +5,4 @@ import de.htwg.se.rummikub.model.tokenComponent.TokenInterface
 trait TokenStructureFactoryInterface {
   def createGroup(tokens: List[TokenInterface]): TokenStructureInterface
   def createRow(tokens: List[TokenInterface]): TokenStructureInterface
-  def tokenStructureFactory: TokenStructureFactoryInterface
-
 }
